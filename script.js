4054ffa7b9d531817d9deae11f08cf7139996dc8
Nikhil-Prabhakar <prabhakarnikhil2003@gmail.com>
2023-07-22 09:05:56 +0530

    JS File

--- script.js
+++ script.js
@@ -13,3 +13,35 @@ function togglemenu() {
 
     }
 }
+const quoteText = document.getElementById('quote'),
+    authorName = document.querySelector(".author .name"),
+    quoteBtn = document.querySelector(".btn2"),
+    soundBtn = document.querySelector(".sound"),
+    copyBtn = document.querySelector(".copy"),
+    twitterBtn = document.querySelector(".twitter");
+function getRandomQuote() {
+    quoteBtn.classList.add("loading")
+    quoteBtn.innerText = "Loading Quote..."
+    fetch('https://api.quotable.io/random')
+        .then(response => response.json())
+        .then(data => {
+            quoteText.innerText = data.content;
+            authorName.innerText = data.author || 'Unknown';
+            quoteBtn.innerText = "New Quote";
+            quoteBtn.classList.remove("loading")
+        })
+        .catch(error => {
+            console.error('Error fetching the quote:', error);
+        });
+}
+soundBtn.addEventListener("click", () => {
+    let utterance = new SpeechSynthesisUtterance(`${quoteText.innerText} by ${authorName.innerText}`);
+    speechSynthesis.speak(utterance);
+});
+copyBtn.addEventListener("click", () => {
+    navigator.clipboard.writeText(quoteText.innerText);
+});
+twitterBtn.addEventListener("click", () => {
+    let tweetUrl = `https://twitter.com/intent/tweet?url=${quoteText.innerText}`;
+    window.open(tweetUrl, "_blank");
+});
